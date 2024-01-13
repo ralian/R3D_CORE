bc09ca85cbc4d377b8883d4e0d04b262eb48c212
Ralian <willb0102@gmail.com>
2024-01-13 18:55:05 -0500

    More work on the thermal components - still unfinished

--- scripts/Game/Vehicles/Sensors/R3D_ThermalEmissionComponent.c
+++ scripts/Game/Vehicles/Sensors/R3D_ThermalEmissionComponent.c
@@ -20,13 +20,37 @@ class R3D_ThermalEmitterComponent: ScriptComponent
 	// = GetGame().GetTimeAndWeatherManager().GetAmbientTemperature(); // someday
 	
 	override void EOnSimulate(IEntity owner, float timeSlice) {
-		// todo some logic here to determine the vehicle engine state 
 		bool power_on = false; 
-		bool max_power = false;
+		float max_heat_fraction = 0.0;
+		
+		auto vehSim = VehicleBaseSimulation.Cast(owner.FindComponent(VehicleBaseSimulation));
+        typename simType = vehSim.Type();
+        
+        switch(simType)
+        {
+            case VehicleWheeledSimulation:
+                power_on = VehicleWheeledSimulation.Cast(vehSim).EngineIsOn();
+                break;
+            case VehicleWheeledSimulation_SA:
+                power_on = VehicleWheeledSimulation_SA.Cast(vehSim).EngineIsOn();
+                break;
+            case VehicleWheeledSimulation_SA_B:
+                power_on = VehicleWheeledSimulation_SA_B.Cast(vehSim).EngineIsOn();
+                break;
+            case VehicleHelicopterSimulation:
+                power_on = VehicleHelicopterSimulation.Cast(vehSim).EngineIsOn();
+                break;
+			// TODO
+            //case ADM_FixedWingSimulation:
+            //    power_on = ADM_FixedWingSimulation.Cast(vehSim).IsEngineOn();
+            //    break;
+            default: 
+                Print("Unsupported vehicle simulation type"); // Or return some default value? (for flares)
+                break;
+        }
 		
 		float target_temp = 274.5;
-		if (power_on) target_temp = T_nominal_K;
-		if (max_power) target_temp = T_max_K;
+		if (power_on) target_temp = T_nominal_K + max_heat_fraction*(T_max_K - T_nominal_K);
 		
 		T_current = T_current + (target_temp - T_current) * (timeSlice / t_heat_transfer);
 	}
--- scripts/Game/Vehicles/Sensors/R3D_ThermalReceiverComponent.c
+++ scripts/Game/Vehicles/Sensors/R3D_ThermalReceiverComponent.c
@@ -19,10 +19,54 @@ class R3D_ThermalReceiverComponent: ScriptComponent
 	[Attribute(desc: "End elev of the search volume in deg", defvalue: "45")]
 	float elev_max;
 	
+	[Attribute(desc: "Seeker angle acceptable error in deg", defvalue: "1")]
+	float angle_error;
+	
 	float azumith_current = 0;
 	float elev_current = 0;
 	
 	R3D_ThermalEmitterComponent target = null;
+	
+	vector GetWSBoresightVector() {
+		vector tform[3];
+		GetOwner().GetWorldTransform(tform);
+		
+		vector seeker_tform[3];
+		Math3D.AnglesToMatrix(Vector(azumith_current, elev_current, 0), seeker_tform);
+		
+		vector result[3];
+		Math3D.MatrixMultiply3(tform, seeker_tform, result);
+		
+		return result[1]; 
+	}
+	
+	R3D_ThermalEmitterComponent GrabBestTarget() {
+		array<IEntity> entities = {};
+		GetGame().GetWorld().GetActiveEntities(entities);
+		
+		vector tform[4];
+		GetOwner().GetWorldTransform(tform);
+		
+		vector boresight_dir = GetWSBoresightVector();
+		
+		R3D_ThermalEmitterComponent hottest = null;
+		foreach (IEntity ent : entities) {
+			array<Managed> emitters = null;
+			ent.FindComponents(R3D_ThermalEmitterComponent, emitters);
+			
+			foreach (Managed entry : emitters) {
+				auto emitter = R3D_ThermalEmitterComponent.Cast(entry);
+				if (emitter) {
+					vector direction = (ent.VectorToParent(emitter.origin) - tform[3]).Normalized();
+					if (vector.Dot(direction, boresight_dir) > Math.Cos(Math.DEG2RAD * angle_error))
+						if (!hottest || hottest.T_current < emitter.T_current)
+							hottest = emitter;
+				}
+			}
+		}
+		
+		return hottest;
+	}
 	
 	override void EOnSimulate(IEntity owner, float timeSlice) {
 		if (!target) return;
